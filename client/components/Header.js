ff55bee9be117a53dc6ce8dc99c7b8779909affb
Stephani McGrath <silentsoundz1977@gmail.com>
2018-03-06 16:38:17 -0800

    works on logout logic

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import query from '../queries/CurretUser'
+import { Link } from 'react-router'
+import mutation from '../mutations/logout'
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({})
+  }
+
+  renderButtons() {
+    const { loading, user } = this.props.data
+
+    if (loading) { return <div /> }
+
+    if (user) {
+      return (
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+      )
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )
+    }
+
+  }
   render() {
-    console.log("(ᗒᗣᗕ) (•̀o•́)ง this.props.data", this.props.data )
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
 
-export default graphql(query)(Header)
+export default graphql(mutation)(
+graphql(query)(Header)
+)
