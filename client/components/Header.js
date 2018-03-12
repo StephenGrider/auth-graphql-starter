6a76cd78bd83fefa4e9e55505f548967d5150ca9
Stephani McGrath <silentsoundz1977@gmail.com>
2018-03-12 08:26:41 -0700

    creates loginform

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,7 +6,9 @@ import mutation from '../mutations/logout'
 
 class Header extends Component {
   onLogoutClick() {
-    this.props.mutate({})
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    })
   }
 
   renderButtons() {
---
+++ client/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import AuthForm from './AuthForm'
+
+class LoginForm extends Component {
+  render () {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm />
+      </div>
+    )
+  }
+}
+
+export default LoginForm
