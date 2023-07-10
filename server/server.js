613026f7de9211cc33b59647044bcbb12173bb17
0x-cygnet <bob@cygnetops.com>
2023-07-10 15:16:17 -0500

    boilerplate updates
    - Bump Mongoose version to 6
    - Bump connect-mongo to version 3
    - Get this boilerplate in line with Lyrical
    - Install Nodemon
    - Update references of mLab to Atlas
    - Delete yarn.lock file
    - Enhanced README

--- server/server.js
+++ server/server.js
@@ -11,33 +11,42 @@ const schema = require('./schema/schema');
 // Create a new Express application
 const app = express();
 
-// Replace with your mongoLab URI
+// Replace with your Mongo Atlas URI
 const MONGO_URI = '';
+if (!MONGO_URI) {
+  throw new Error('You must provide a Mongo Atlas URI');
+}
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
+mongoose.set('strictQuery', false);
+
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
+  .on('error', (error) =>
+    console.log('Error connecting to Mongo Atlas:', error)
+  );
 
 // Configures express to use sessions.  This places an encrypted identifier
 // on the users cookie.  When a user makes a request, this middleware examines
 // the cookie and modifies the request object to indicate which user made the request
 // The cookie itself only contains the id of a session; more data about the session
 // is stored inside of MongoDB.
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'aaabbbccc',
-  store: new MongoStore({
-    url: MONGO_URI,
-    autoReconnect: true
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'aaabbbccc',
+    store: new MongoStore({
+      url: MONGO_URI,
+      autoReconnect: true
+    })
   })
-}));
+);
 
 // Passport is wired into express as a middleware. When a request comes in,
 // Passport will examine the request's session (as set by the above config) and
@@ -47,10 +56,13 @@ app.use(passport.session());
 
 // Instruct Express to pass on any request made to the '/graphql' route
 // to the GraphQL instance.
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 // Webpack runs as a middleware.  If any request comes in for the root route ('/')
 // Webpack will respond with the output of the webpack process: an HTML file and
